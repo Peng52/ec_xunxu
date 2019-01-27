e62809a8574e759d65f48f5f69282b6187f947ad
Jack <pengcheng1161922543@gmail.com>
2019-01-27 23:15:47 +0800

    1-28

--- src/main/java/com/peng/xunwu/controller/HelloController.java
+++ src/main/java/com/peng/xunwu/controller/HelloController.java
@@ -1,8 +1,8 @@
 package com.peng.xunwu.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @program: xunwu
@@ -11,15 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @create: 2019-01-26 18:12
  **/
 
-@RestController
+@Controller
 @RequestMapping("/hello")
 public class HelloController {
     @RequestMapping(value = "/error",method = RequestMethod.GET)
     public String error(){
         System.out.println("405");
-        System.out.println("grg");
-        System.out.println("git checkout");
-        return "uuuuu";
+
+        return "404";
     }
 
 
